b0f24f6a6a00e47c9f26d914310b6807e28e924a
mourapi <84185074+mourapi@users.noreply.github.com>
2023-10-20 17:54:30 -0300

    First Steps in Java with Spring Boot 3

---
+++ 06_EntendendoPaths/src/main/java/br/com/fsm/apptest/Greenting.java
@@ -0,0 +1,22 @@
+package br.com.fsm.apptest;
+
+public class Greenting {
+
+    private final long id;
+    private final String content;
+
+    public Greenting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+        
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
---
+++ 06_EntendendoPaths/src/main/java/br/com/fsm/apptest/MathController.java
@@ -0,0 +1,42 @@
+package br.com.fsm.apptest;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+
+    //private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}",
+          method=RequestMethod.GET)
+    public Double sum(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws Exception{
+      if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+          throw new Exception();
+      }
+      return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }  
+
+    private Double convertToDouble(String strNumber) {
+      if (strNumber == null) return 0D;
+      // BR 10,25 US 10.25
+      String number = strNumber.replaceAll(",", ".");
+      if (isNumeric(number)) return Double.parseDouble(number);
+      return 0D;
+    }
+
+    private boolean isNumeric(String strNumber) {
+      if (strNumber == null) return false;
+      String number = strNumber.replaceAll(",", ".");
+      return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+    }
+    
