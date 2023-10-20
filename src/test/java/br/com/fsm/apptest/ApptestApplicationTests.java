d597cfb147945b386bd77253928e71d297778506
mourapi <84185074+mourapi@users.noreply.github.com>
2023-10-20 11:46:19 -0300

    First Steps in Java with Spring Boot 3

---
+++ src/test/java/br/com/fsm/apptest/ApptestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fsm.apptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApptestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
